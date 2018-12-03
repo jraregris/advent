af5e921dca79361c76829f77556817e047438eeb
oddmunds <oddmunds@iterate.no>
2018-12-03 22:26:27 +0100

    day 1 part 2

---
+++ lib/change.ex
@@ -0,0 +1,15 @@
+defmodule Change do
+  def start(n) do
+    Agent.start_link(fn -> n end)
+  end
+
+  def next(f) do
+    [h | tail] = Agent.get(f, fn n -> n end)
+    Agent.update(f, fn _ -> tail ++ [h] end)
+    h
+  end
+
+  def stop(f) do
+    Agent.stop(f)
+  end
+end
---
+++ lib/freqs.ex
@@ -0,0 +1,21 @@
+defmodule Freqs do
+  def start do
+    Agent.start_link(fn -> MapSet.new([0]) end)
+  end
+
+  def add(f, n) do
+    Agent.update(f, fn freqs ->
+      MapSet.put(freqs, n)
+    end)
+  end
+
+  def has?(f, n) do
+    Agent.get(f, fn freqs ->
+      MapSet.member?(freqs, n)
+    end)
+  end
+
+  def stop(f) do
+    Agent.stop(f)
+  end
+end
