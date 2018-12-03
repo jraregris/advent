103b1edfaf34ecfee9dab22e5a78a0220f1262f7
oddmunds <oddmunds@iterate.no>
2018-12-03 20:38:10 +0100

    puzzle1

--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -2,7 +2,7 @@ defmodule AdventTest do
   use ExUnit.Case
 
   test "greets the world" do
-    frequeny = [+1, -2, +3, +1] |> Day1.calibrate()
+    frequency = [+1, -2, +3, +1] |> Day1.calibrate()
     assert 3 == 3
   end
 end
