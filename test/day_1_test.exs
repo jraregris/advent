92593c41948ea4adcd608bfddc74a0493c8a316c
oddmunds <oddmunds@iterate.no>
2018-12-03 20:48:02 +0100

    puzzle1

--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -1,23 +1,25 @@
 defmodule AdventTest do
   use ExUnit.Case
 
-  test "first example" do
-    frequency = [+1, -2, +3, +1] |> Day1.calibrate()
-    assert 3 == frequency
-  end
+  describe "Examples" do
+    test "first example" do
+      frequency = [+1, -2, +3, +1] |> Day1.calibrate()
+      assert 3 == frequency
+    end
 
-  test "second example" do
-    frequency = [+1, +1, +1] |> Day1.calibrate()
-    assert 3 == frequency
-  end
+    test "second example" do
+      frequency = [+1, +1, +1] |> Day1.calibrate()
+      assert 3 == frequency
+    end
 
-  test "third example" do
-    frequency = [+1, +1, -2] |> Day1.calibrate()
-    assert frequency == 0
-  end
+    test "third example" do
+      frequency = [+1, +1, -2] |> Day1.calibrate()
+      assert frequency == 0
+    end
 
-  test "fourth example" do
-    frequency = [-1, -2, -3] |> Day1.calibrate()
-    assert frequency == -6
+    test "fourth example" do
+      frequency = [-1, -2, -3] |> Day1.calibrate()
+      assert frequency == -6
+    end
   end
 end
