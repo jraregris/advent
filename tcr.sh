27c2017c34b2d023f4e0fc6a48830c3df5031eef
oddmunds <oddmunds@iterate.no>
2018-12-04 19:08:46 +0100

    tcr

--- tcr.sh
+++ tcr.sh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 mix test --exclude pending \
-  && git commit -am $1 || git reset --hard && mix test --only pending
+  && git commit -am $1 \
+  || git reset --hard \
+  && mix test --only pending
