daf2a302c0cb384eda5826c6ae96a0929af3b3d5
oddmunds <oddmunds@iterate.no>
2018-12-14 00:55:09 +0100

    day 3 part 1

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -1,10 +1,13 @@
 defmodule Day3 do
   def parse(s) do
-    [_, id, x] = Regex.run(~r/#(\d+) @ (\d+)/, s)
+    [_, id, x, y] =
+      Regex.run(~r/#(\d+) @ (\d+),(\d+)/, s)
 
     %Claim{
       id: id,
-      position: {x |> String.to_integer()}
+      position:
+        {x |> String.to_integer(),
+         y |> String.to_integer()}
     }
   end
 end
