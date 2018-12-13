dbf671f348686a40be63414d319e34b30f2f9159
oddmunds <oddmunds@iterate.no>
2018-12-14 00:29:31 +0100

    day 3 part 1

--- tcr.sh
+++ tcr.sh
@@ -3,4 +3,4 @@ clear
 mix test --exclude pending \
   && git commit -am $1 1>/dev/null \
   || git reset --hard \
-  && grep -r "@tag :pending" test && mix test --only pending && echo "\nIt works, remove '@tag :pending@, ya bobo!'"
+  && grep -r "@tag :pending" . && mix test --only pending && echo "\nIt works, remove '@tag :pending@, ya bobo!'"
