6632e609a8b194b50f5cd4c3958146c07b0715cd
oddmunds <oddmunds@iterate.no>
2018-12-14 00:54:19 +0100

    day 3 part 1

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -1,6 +1,10 @@
 defmodule Day3 do
   def parse(s) do
     [_, id, x] = Regex.run(~r/#(\d+) @ (\d+)/, s)
-    %Claim{id: id, position: {x}}
+
+    %Claim{
+      id: id,
+      position: {x |> String.to_integer()}
+    }
   end
 end
