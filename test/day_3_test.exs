9d45e0a53fc513086efc2011f72db1135b3ddcb3
oddmunds <oddmunds@iterate.no>
2018-12-14 00:45:33 +0100

    day 3 part 1

--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -3,7 +3,7 @@ defmodule Day3Test do
 
   @tag :pending
   test "parse claim id" do
-    claim = Day2.parse("#123 @ 3,2: 5x4")
+    claim = Day3.parse("#123 @ 3,2: 5x4")
     assert claim.id == 123
   end
 end
