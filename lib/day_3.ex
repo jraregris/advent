09c222b99b0f3ab8fd269281b2107ef8e7b1c3a5
oddmunds <oddmunds@iterate.no>
2018-12-14 01:00:13 +0100

    day 3 part 1

--- lib/day_3.ex
+++ lib/day_3.ex
@@ -1,8 +1,8 @@
 defmodule Day3 do
   def parse(s) do
-    [_, id, x, y, width] =
+    [_, id, x, y, width, height] =
       Regex.run(
-        ~r/#(\d+) @ (\d+),(\d+): (\d+)x/,
+        ~r/#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/,
         s
       )
 
@@ -11,7 +11,9 @@ defmodule Day3 do
       position:
         {x |> String.to_integer(),
          y |> String.to_integer()},
-      size: {width}
+      size:
+        {width |> String.to_integer(),
+         height |> String.to_integer()}
     }
   end
 end
