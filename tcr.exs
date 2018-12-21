893bfb9d15c9801c16b55412aecbb45b42fc82d9
oddmunds <oddmunds@iterate.no>
2018-12-21 21:21:08 +0100

    tcr

---
+++ tcr.exs
@@ -0,0 +1,41 @@
+defmodule TCR do
+  def clear() do
+    IO.ANSI.format([:clear, :home]) |> IO.write()
+  end
+
+  def test(opts \\ %{pending: false}) do
+    pending = opts[:pending]
+
+    IO.ANSI.format([:inverse, "TEST"]) |> IO.puts()
+
+    {_, status} =
+      System.cmd(
+        "mix",
+        ["test"],
+        into: IO.stream(:stdio, 1)
+      )
+
+    case status do
+      0 ->
+        :ok
+
+      _ ->
+        :fail
+    end
+  end
+
+  def commit() do
+    IO.ANSI.format([:inverse, "COMMIT"]) |> IO.puts()
+    System.cmd("git", ["add", "."], into: IO.stream(:stdio, 1)) |> IO.inspect()
+
+    System.cmd("git", ["commit", "--message", "tcr"], into: IO.stream(:stdio, 1))
+    |> IO.inspect()
+  end
+end
+
+TCR.clear()
+test = TCR.test()
+
+if test == :ok do
+  commit = TCR.commit()
+end
