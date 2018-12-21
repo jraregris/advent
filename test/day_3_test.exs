caaa313265887c42d8ab520a2c9cc29b7706ca2d
oddmunds <oddmunds@iterate.no>
2018-12-22 00:21:30 +0100

    tcr

--- test/day_3_test.exs
+++ test/day_3_test.exs
@@ -15,4 +15,10 @@ defmodule Day3Test do
     claim = Day3.parse("#123 @ 3,2: 5x4")
     assert claim.size == {5, 4}
   end
+
+  @tag :pending
+  test "parse claim width honk" do
+    claim = Day3.parse("#123 @ 3,2: 5x4")
+    assert claim.size == {5, 4}
+  end
 end
