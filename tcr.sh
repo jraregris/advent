d13329f20f654bbfe7e2481940b8dc616bfa8fa7
oddmunds <oddmunds@iterate.no>
2018-12-22 02:18:51 +0100

    tcr

--- tcr.sh
+++ tcr.sh
@@ -1,7 +1,2 @@
 #!/bin/zsh
-clear
-mix test --exclude pending \
-  && git add . \
-  && git commit -m $1 1>/dev/null \
-  || git reset --hard \
-  && grep -r "@tag :pending" test && mix test --only pending && echo "\nIt works, remove '@tag :pending@, ya bobo!'"
+elixir tcr.exs
