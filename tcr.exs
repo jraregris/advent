50017aea4463d6f6576dbac1fc0dd8c0f0618517
oddmunds <oddmunds@iterate.no>
2018-12-22 01:23:16 +0100

    tcr

--- tcr.exs
+++ tcr.exs
@@ -1,7 +1,7 @@
 defmodule TCR do
   def clear() do
     IO.ANSI.format([:clear, :home]) |> IO.write()
-    IO.ANSI.format([:], emit? \\ enabled?())
+    IO.ANSI.format([:light_black, DateTime.utc_now() |> to_string]) |> IO.puts()
   end
 
   def test() do
