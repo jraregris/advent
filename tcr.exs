bb98cbc41a47a2986e5d0b1554351206a2921e37
oddmunds <oddmunds@iterate.no>
2019-01-04 12:23:24 +0100

    tcr

--- tcr.exs
+++ tcr.exs
@@ -25,6 +25,8 @@ defmodule Term do
       DateTime.utc_now()
       |> DateTime.diff(start_time, :milliseconds)
 
+    IO.ANSI.cursor(1, 29) |> IO.write()
+
     IO.ANSI.format([" ", duration |> to_string, "ms"])
     |> IO.write()
   end
