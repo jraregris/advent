0a283ad11442aaf544deeecc1e734b8d08a7e198
oddmunds <oddmunds@iterate.no>
2019-01-04 16:06:31 +0100

    tcr

--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -51,9 +51,10 @@ defmodule Day1Test do
       assert frequency == 5
     end
 
+    @tag :pending
     test "fourth example" do
       frequency = [+7, +7, -2, -7, -4] |> Day1.frequency()
-      assert frequency == 14
+      assert frequency == 16
     end
   end
 end
