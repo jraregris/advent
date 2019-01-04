3c9fd7de3b5d8dbd2f9032fdafd6e8ec1942628e
oddmunds <oddmunds@iterate.no>
2019-01-04 12:31:48 +0100

    tcr

--- tcr.exs
+++ tcr.exs
@@ -18,7 +18,7 @@ defmodule Term do
   def hl() do
     IO.ANSI.cursor(3, 0) |> IO.write()
 
-    {cols, 0} = System.cmd("tput", ["cols"]) |> IO.inspect()
+    {cols, 0} = System.cmd("tput", ["cols"])
     {n, _} = cols |> Integer.parse()
     List.duplicate("─", n) |> Enum.join() |> IO.write()
   end
